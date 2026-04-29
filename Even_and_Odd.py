baff79855479e3dc53527ce8224e9231c10f6d6b
Manesh <manishbhagane127@gmail.com>
2026-04-29 14:08:01 +0530

    Add files via upload

---
+++ Even_and_Odd.py
@@ -0,0 +1,9 @@
+even_number = []
+odd_number = []
+for i in range(0,100):
+      if i % 2 == 0:
+          even_number.append(i)
+      else:
+           odd_number.append(i)
+print (even_number)
+print (odd_number)
